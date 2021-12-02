9383302089deb1e254796f8ebc8d551be8c3edb2
lvjiong <64924372+lvjiong@users.noreply.github.com>
2021-12-02 22:32:11 +0800

    Update BaiduProxyNew.lua

--- BaiduProxyNew.lua
+++ BaiduProxyNew.lua
@@ -47,7 +47,7 @@ function wa_lua_on_handshake_cb(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
                     'Host: ' .. host .. ':' .. port .. '\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
-                    'X-T5-Auth: YTY0Nzlk\r\n\r\n'
+                    'X-T5-Auth: ZjQxNDIh\r\n\r\n'
         ctx_write(ctx, res)
         flags[uuid] = kHttpHeaderSent
     end
