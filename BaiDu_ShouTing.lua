8d836e1000ac0e0617e706e6611514bb8a7f2968
lvjiong <64924372+lvjiong@users.noreply.github.com>
2021-12-04 13:00:59 +0800

    Update BaiDu_ShouTing.lua

--- BaiDu_ShouTing.lua
+++ BaiDu_ShouTing.lua
@@ -45,8 +45,8 @@ function wa_lua_on_handshake_cb(ctx)
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n\r' ..
-                    'Host: shoutingtoutiao3.10010.com:80\r\n' ..
                     'Host: ' .. host .. ':' .. port .. '\r\n' ..
+                    'Host: shoutingtoutiao3.10010.com:80\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: YTY0Nzlk\r\n\r\n'
         ctx_write(ctx, res)
