3789b4ca32823ef6200534ac1e809cfa58585777
lvjiong <64924372+lvjiong@users.noreply.github.com>
2021-12-05 16:05:03 +0800

    Update UC.lua

--- UC.lua
+++ UC.lua
@@ -44,7 +44,8 @@ function wa_lua_on_handshake_cb(ctx)
     if flags[uuid] ~= kHttpHeaderSent then
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
-        local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
+        local res = 'CONNECT applog.uc.cn:443/ HTTP/1.1\r\n' ..
+                    'Proxy-Authorization: Basic dWMxMC4xOTQuMTg3LjIyMDoxZjQ3ZDNlZjUzYjAzNTQ0MzQ1MWM3ZWU3ODczZmYzOA==\r\n
                     'Host: ' .. host .. ':' .. port .. '\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: YTY0Nzlk\r\n\r\n'
